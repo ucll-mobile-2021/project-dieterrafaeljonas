6a7e22e97ad796186d93df436456ffbf67d24ac5
JonasDeBoeck <Jonas.deboeck@student.ucll.be>
2020-10-04 15:24:17 +0200

    Swipe animatie, begin

---
+++ app/src/main/java/com/example/projectmobiledev/Activity2.kt
@@ -0,0 +1,13 @@
+package com.example.projectmobiledev
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
