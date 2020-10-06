2db62451f8d6845a5635fefabc071951799046ed
RafaelBackx <rafaelbackx@live.be>
2020-10-06 14:33:48 +0200

    added start on current location

--- app/src/main/java/com/example/projectmobiledev/tracker/Tracker.kt
+++ app/src/main/java/com/example/projectmobiledev/tracker/Tracker.kt
@@ -1,5 +1,6 @@
 package com.example.projectmobiledev.tracker
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Color
@@ -11,7 +12,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.projectmobiledev.R
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -26,6 +29,7 @@ class Tracker : AppCompatActivity(), LocationListener, OnMapReadyCallback {
     private lateinit var polyLineOptions : PolylineOptions
     private lateinit var line : Polyline
     private val controller : TrackerController = TrackerController()
+    private lateinit var locationProvider : FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,16 +40,31 @@ class Tracker : AppCompatActivity(), LocationListener, OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        locationProvider = LocationServices.getFusedLocationProviderClient(this)
+
         polyLineOptions = PolylineOptions()
         polyLineOptions.width(5f);
         polyLineOptions.color(Color.MAGENTA)
     }
 
-    private fun checkLocationPermission(){
+    private fun checkLocationPermission() {
         // check if we already have location permission
         if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
             // if not ask for location permission
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun startOnCurrentLocation(){
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationProvider.lastLocation.addOnSuccessListener(this)
+            { location ->
+                if (location != null){
+                    val loc:LatLng = LatLng(location.latitude,location.longitude)
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc,18f))
+                }
+            }
         }
     }
 
@@ -62,6 +81,7 @@ class Tracker : AppCompatActivity(), LocationListener, OnMapReadyCallback {
         val locationRequests = LocationRequest()
         locationRequests.interval = 5000
         locationRequests.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        startOnCurrentLocation()
     }
 
     private fun drawMap(){
