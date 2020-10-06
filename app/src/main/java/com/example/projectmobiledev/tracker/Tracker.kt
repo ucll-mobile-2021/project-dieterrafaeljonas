6214a23f000553fba6de9c1b6017c336b0c58f86
RafaelBackx <rafaelbackx@live.be>
2020-10-06 10:33:36 +0200

    added google maps dependencies

---
+++ app/src/main/java/com/example/projectmobiledev/tracker/Tracker.kt
@@ -0,0 +1,14 @@
+package com.example.projectmobiledev.tracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectmobiledev.R
+
+class Tracker : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tracker)
+    }
+
+}
