d2770dbb19a85e296d2a4a031b6a6f95aa553234
Pieter-JanVanDerSchueren-r0465971 <32223183+Pieter-JanVanDerSchueren-r0465971@users.noreply.github.com>
2020-10-13 18:10:35 +0200

    Firebase initialization
    First steps taken towards integrating a realtime database with Firebase.

--- app/src/main/java/com/example/projectmobiledev/MainActivity.kt
+++ app/src/main/java/com/example/projectmobiledev/MainActivity.kt
@@ -1,16 +1,58 @@
 package com.example.projectmobiledev
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.GestureDetector
-import android.view.MotionEvent
-import kotlin.math.abs
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate (savedInstanceState: Bundle?) {
+    companion object {
+        private const val TAG = "KotlinActivity"
+    }
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val obj = FirebaseDatabase.getInstance()
+        val mDatabase = obj.reference
+        for (i in 0..9) {
+            val id = i.toString() + ""
+            mDatabase.child("users").child(id).setValue("TEST")
+        }
+    }
+
+    fun basicReadWrite() {
+        // [START write_message]
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+        // [END write_message]
+
+        // [START read_message]
+        // Read from the database
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue<String>()
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+        // [END read_message]
     }
 }
