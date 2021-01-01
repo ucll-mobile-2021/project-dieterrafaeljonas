8d91c7cae1c91efa1e997bb1aa5d5886ae7c03cf
RafaelBackx <rafaelbackx@live.be>
2021-01-01 21:27:37 +0100

    added permission to manifest removed old network code

--- app/src/main/java/com/example/projectmobiledev/tracker/RouteViewer.kt
+++ app/src/main/java/com/example/projectmobiledev/tracker/RouteViewer.kt
@@ -58,14 +58,6 @@ class RouteViewer() : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarke
     lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
-//        if(!isOnline(this)){
-//            val inflater = layoutInflater
-//            val popup = AlertDialog.Builder(this)
-//            val view = inflater.inflate(R.layout.internet_alert, null)
-//            popup.setView(view)
-//            popup.show()
-//        }
-
         val connectivityManager = getSystemService(ConnectivityManager::class.java)
         val networkListener = NetworkListener(this,layoutInflater)
         connectivityManager.registerDefaultNetworkCallback(networkListener)
@@ -215,27 +207,5 @@ class RouteViewer() : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarke
             return true;
         }
         return false;
-    }
-
-    private fun isOnline(context: Context): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (connectivityManager != null) {
-            val capabilities =
-                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-            if (capabilities != null) {
-                if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
-                    return true
-                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
-                    return true
-                } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
-                    Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
-                    return true
-                }
-            }
-        }
-        return false
     }
 }
